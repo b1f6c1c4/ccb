8a03946d0d2206326af34e48906fa65d67e8a32a
b1f6c1c4 <b1f6c1c4@gmail.com>
2022-02-14 21:25:38 -0500

    upload

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/bin/env node
+
+const fs = require('fs');
+const emlformat = require('eml-format');
+
+const eml = fs.readFileSync(process.argv[2] || 0, 'utf-8');
+emlformat.read(eml, (error, data) => {
+  if (error)
+    return console.log(error);
+  if (!data.text)
+    return;
+  const t = data.text.replace(/\r\n/g, '\n');
+  const re = /^ (?<td>20[0-9][0-9]-[01][0-9]-[012][0-9])\s\s(?<pd>20[0-9][0-9]-[01][0-9]-[012][0-9])\s\s(?:[0-9]{4})\s\s(?<rmk>\S+(?:\s\S+)*)\s\s(?<tc>[A-Z]{3})\s\s(?<tf>-?[0-9]{1,3}(?:,[0-9]{3})*\.[0-9]{2})\s\s(?<pc>[A-Z]{3})\s\s(?<pf>-?[0-9]{1,3}(?:,[0-9]{3})*\.[0-9]{2})$/gm;
+  let m;
+  console.log('Transaction Date,Remark,Transaction Currency,Amount');
+  while (m = re.exec(t)) {
+    const o = {
+      ...m.groups,
+      tf: +m.groups.tf.replace(/,/, ''),
+      bf: +m.groups.pf.replace(/,/, ''),
+    };
+    console.log(`${o.td},${o.rmk},${o.tc},${o.tf}`);
+  }
+});
